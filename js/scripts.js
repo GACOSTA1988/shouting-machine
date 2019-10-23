7930a5471a9894124ee5df3f45e814062ff2f65a
Student <example@example.com>
2019-10-23 13:53:39 -0700

    add uppercase function to variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var contentInput = $("input#content").val ();
+    contentInput=contentInput.toUpperCase();
     $(".content").text(contentInput);
-
     $("#story").show();
-
-
 
     event.preventDefault();
   });
